5d06f3c211b6a23e628b54020fcdca0db236ba58
Ramiro Crespo <ramac1008@gmail.com>
2023-01-13 16:38:33 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ resource "time_sleep" "wait_30_seconds" {
 
 module "gke_auth" {
   depends_on           = [time_sleep.wait_30_seconds]
-  source               = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+  source               = "terraform-google-modules/kubernetes-engine/google/modules/auth"
   project_id           = var.project_id
   cluster_name         = google_container_cluster.main.name
   location             = var.location
