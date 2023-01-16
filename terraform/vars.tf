4b7db18542eee67a1adeb9bf662164964e03a653
Ramiro Crespo <ramac1008@gmail.com>
2023-01-16 11:58:04 -0400

    ++2 name: argocd  default   = "k8s-cluster-2"

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -7,7 +7,7 @@ variable "project_id" {
 variable "cluster_name" {
   type      = string
   description = "cluster name"
-  default   = "k8s-cluster"
+  default   = "k8s-cluster-2"
 }
 
 variable "region" {
